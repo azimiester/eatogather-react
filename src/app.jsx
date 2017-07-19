9b5a4f4d13ca3097b2a0bd4b0940d9f371572488
Akhter Syed <akhters1@niksula.hut.fi>
2017-07-19 18:28:50 +0300

    Settings

--- src/app.jsx
+++ src/app.jsx
@@ -1,2 +1,8 @@
-console.log('hi');
-var a = 0;
+import React from 'react';
+import { render } from 'react-dom';
+
+const renderApp = () => {
+	render(<h1>Hello, world!</h1>, document.getElementById('app'));
+};
+
+renderApp();
